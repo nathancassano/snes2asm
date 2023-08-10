64336b66a6f41125230d78020d7e8f86780bd2d0
Nathan <nathan@bubblezapgames.com>
2023-08-10 15:47:35 -0700

    Ensure project is assembled using wla-dx 10.0 or higher

--- snes2asm/template/main.s
+++ snes2asm/template/main.s
@@ -1,3 +1,11 @@
+.ifdef WLA_VERSION
+.if WLA_VERSION < "10.0"
+.fail "Invalid version of wla-dx. Please ensure you are using version 10.0 or higher"
+.endif
+.else
+.fail "Invalid version of wla-dx. Please ensure you are using version 10.0 or higher"
+.endif
+
 .include "hdr.asm"
 .include "snes.asm"
 .include "game.asm"
